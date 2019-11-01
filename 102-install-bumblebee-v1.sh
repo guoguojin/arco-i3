07eda87faff12f7e845b1dc04415395f49a941dc
Tan Quach <t@nqua.ch>
2019-11-01 17:06:58 +0000

    Updated optimus installation

--- 102-install-bumblebee-v1.sh
+++ 102-install-bumblebee-v1.sh
@@ -4,9 +4,10 @@ set -e
 # Author	:	Tan Quach
 
 # Bumblebee for optimus enabled laptops
-sudo pacman -S --noconfirm --needed bumblebee
+sudo pacman -S --noconfirm --needed bumblebee bbswitch
+sudo pacman -S --noconfirm --needed optimus-manager
 
-sudo systemctl enable bumblebee
+sudo systemctl enable optimus-manager.service
 
 echo "################################################################"
 echo "##############   bumblebee software installed   ################"
